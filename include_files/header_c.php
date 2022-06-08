6c3f444a089cf144294afbecb76f8c4e279f897e
nilarhtay <nilar.htay@spiceworks.co.jp>
2022-06-08 14:37:26 +0630

    mypage header update
    mypage header update

--- include_files/header_c.php
+++ include_files/header_c.php
@@ -9,82 +9,72 @@ $url =$_SERVER['PHP_SELF'];
     <div class="header-right md">
         <div class="content-top">
             <ul class="list-function function-mypage">
-                <li class="item-function"><a class="trans item-link icon01" href="#">WEBタイムシート</a></li>
-                <li class="item-function"><a class="trans item-link icon02" href="#">WEB給与明細</a></li>
-                <li class="item-function"><a class="trans item-link icon03" href="#">ログアウト</a></li>
+                <li class="item-function item-function01"><a class="trans item-link" href="#">WEBタイムシート</a></li>
+                <li class="item-function item-function02"><a class="trans item-link" href="#">WEB給与明細</a></li>
+                <li class="item-function item-function03"><a class="trans item-link" href="#">ログアウト</a></li>
             </ul>
         </div>
     </div>
 </div>
-<div class="content-list-menu list-service-menu menu-mypage md">
-    <ul class="list-menu-top">
-        <li class="item-list-top <?php echo ($url == '/mypage/index.html')? 'active-hover':''; ?>"><a href="../mypage/">ホーム</a></li>
-        <li class="item-list-top"><a href="#">お仕事検索</a></li>
-        <li class="item-list-top"><a href="#">キャリアアップ支援制度</a></li>
-        <li class="item-list-top"><a href="#">福利厚生</a></li>
-        <li class="item-list-top <?php echo ($url == '/mypage/application.html')? 'active-hover':''; ?>"><a href="../mypage/application.html">規定・各種申請書</a></li>
-        <li class="item-list-top"><a href="#">各種お問い合わせ</a></li>
-    </ul>
-</div>
-<nav class="menu-navigation js-navigation sm">
-    <ul class="list-function function-mypage">
-        <li class="item-function"><a class="trans item-link icon01" href="#">WEBタイムシート</a></li>
-        <li class="item-function"><a class="trans item-link icon02" href="#">WEB給与明細</a></li>
-        <li class="item-function"><a class="trans item-link icon03" href="#">ログアウト</a></li>
-    </ul>
+<nav class="menu-navigation js-navigation mypage-navigation">
     <div class="navigation-block">
+        <ul class="list-function function-mypage">
+            <li class="item-function"><a class="trans item-link icon01" href="#">WEBタイムシート</a></li>
+            <li class="item-function"><a class="trans item-link icon02" href="#">WEB給与明細</a></li>
+            <li class="item-function"><a class="trans item-link icon03" href="#">ログアウト</a></li>
+        </ul>
+        <div class="item-menu top_item">
+            <div class="title-menu js-accordion">
+                <a class="trans js-accordion-heading" href="./">マイページTOP</a>
+            </div>
+        </div>
         <ul class="header-menu">
             <li class="item-menu">
                 <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="./">マイページTOP</a>
+                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon sm" src="../img/mypage/sp/mypage_top_img_01.png" alt="お仕事検索">お仕事検索</a>
                 </div>
             </li>
             <li class="item-menu">
                 <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon" src="../img/mypage/sp/mypage_top_img_01.png" alt="お仕事検索">お仕事検索</a>
+                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon sm" src="../img/mypage/sp/mypage_top_img_02.png" alt="キャリアアップ支援制度">キャリアアップ支援制度</a>
                 </div>
             </li>
             <li class="item-menu">
                 <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon" src="../img/mypage/sp/mypage_top_img_02.png" alt="キャリアアップ支援制度">キャリアアップ支援制度</a>
+                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon sm" src="../img/mypage/sp/mypage_top_img_03.png" alt="お友達紹介（全エリア）">お友達紹介（全エリア）</a>
                 </div>
             </li>
             <li class="item-menu">
                 <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon" src="../img/mypage/sp/mypage_top_img_03.png" alt="お友達紹介（全エリア）">お友達紹介（全エリア）</a>
+                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon sm" src="../img/mypage/sp/mypage_top_img_04.png" alt="福利厚生">福利厚生</a>
                 </div>
             </li>
             <li class="item-menu">
                 <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon" src="../img/mypage/sp/mypage_top_img_04.png" alt="福利厚生">福利厚生</a>
+                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon sm" src="../img/mypage/sp/mypage_top_img_05.png" alt="規定・各種申請書">規定・各種申請書</a>
                 </div>
             </li>
             <li class="item-menu">
                 <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon" src="../img/mypage/sp/mypage_top_img_05.png" alt="規定・各種申請書">規定・各種申請書</a>
+                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon sm" src="../img/mypage/sp/mypage_top_img_06.png" alt="各種限定キャンペーン">各種限定キャンペーン</a>
                 </div>
             </li>
             <li class="item-menu">
                 <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon" src="../img/mypage/sp/mypage_top_img_06.png" alt="各種限定キャンペーン">各種限定キャンペーン</a>
+                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon sm" src="../img/mypage/sp/mypage_top_img_07.png" alt="登録情報">登録情報</a>
                 </div>
             </li>
             <li class="item-menu">
                 <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon" src="../img/mypage/sp/mypage_top_img_07.png" alt="登録情報">登録情報</a>
+                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon sm" src="../img/mypage/sp/mypage_top_img_08.png" alt="各種お問合せ">各種お問合せ</a>
                 </div>
             </li>
             <li class="item-menu">
                 <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon" src="../img/mypage/sp/mypage_top_img_08.png" alt="各種お問合せ">各種お問合せ</a>
-                </div>
-            </li>
-            <li class="item-menu">
-                <div class="title-menu js-accordion">
-                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon" src="../img/mypage/sp/mypage_top_img_09.png" alt="法律関連">法律関連</a>
+                    <a class="trans js-accordion-heading" href="#"><img class="menu_icon sm" src="../img/mypage/sp/mypage_top_img_09.png" alt="法律関連">法律関連</a>
                 </div>
             </li>
         </ul>
     </div>
 </nav>
-<div class="button-menu trans js-button-menu sm"><span></span><span></span><span></span></div>
+<div class="button-menu trans js-button-menu"><span></span><span></span><span></span></div>
