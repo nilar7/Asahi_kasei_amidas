3eb0efc3a07ddd6adf6a1c06b2a352c54e462428
syhspiceworks <76146106+syhspiceworks@users.noreply.github.com>
2022-06-30 11:41:31 +0630

    243 to 246

---
+++ include_files/contact_e.php
@@ -0,0 +1,15 @@
+<?php
+$url =$_SERVER['PHP_SELF'];
+?>
+<div class="footer-top footer-different">
+    <div class="wrapper">
+        <h2 class="title-contact"><span class="title-english font-alata">CONTACT</span><span class="title-japan">お問い合わせ</span></h2>
+        <p class="text-contact">人材派遣サービスに関して、<br>気になることがある方は<br class="sm">お気軽にお問い合わせください。</p>
+        <ul class="list-inquiry">
+            <li class="item-inquiry button-big"><a href="#">資料請求・お問い合わせ</a></li>
+            <li class="item-inquiry button-small">
+            <p class="text-inquiry-small"><span>人材派遣に関するお役立ち資料を<br class="sm">無料でご活用いただけます</span></p><a href="#">お役立ち資料ダウンロード</a>
+            </li>
+        </ul>
+    </div>
+</div>
